1274a4093ac7d2e307eb245564a499404300b03c
유용태 <teo.v@navercorp.com>
2026-06-14 10:36:57 +0900

    Use Figma clone as default preview

--- src/CanvasRoot.tsx
+++ src/CanvasRoot.tsx
@@ -1,8 +1,17 @@
 import { CanvasDevToolsDemoApp } from './demo/CanvasDevToolsDemoApp'
 import { DEMO_CANVAS_APP_ASSEMBLY_INPUT } from './demo/CanvasDemoAssembly'
+import { FigmaCloneApp } from '../packages/figma-clone/src'
 
 export function CanvasRoot() {
+  const demo = new URLSearchParams(window.location.search).get('demo')
+
+  if (demo === 'engine') {
+    return (
+      <CanvasDevToolsDemoApp assemblyInput={DEMO_CANVAS_APP_ASSEMBLY_INPUT} />
+    )
+  }
+
   return (
-    <CanvasDevToolsDemoApp assemblyInput={DEMO_CANVAS_APP_ASSEMBLY_INPUT} />
+    <FigmaCloneApp />
   )
 }
